0ac971ad9127e6c8d07806ea8aacb798bd2fc2ce
Rafael França <rafael@exacta.inf.puc-rio.br>
2026-03-02 12:54:59 -0300

    1.7 - adding some more ids and lcoales

--- CreatureDisplayDBzoneFixed.lua
+++ CreatureDisplayDBzoneFixed.lua
@@ -7,17 +7,34 @@
 CreatureDisplayDBzoneFixed = {}
 CreatureDisplayDBzoneFixed.byZoneName = {}
 
-CreatureDisplayDBzoneFixed.byZoneName[ [=[The Maw]=] ] = {
-    [0] = {
-        [ [=[Thrall]=] ] = 166981,
-        [ [=[Lady Jaina Proudmoore]=] ] = 166980,
-        [ [=[Baine Bloodhoof]=] ] = 168162,
-        [ [=[The Jailer]=] ] = 165539,   
+CreatureDisplayDBzoneFixed.byZoneName = { 
+    [ [=[The Maw]=] ] = {
+        [0] = {
+            [ [=[Thrall]=] ] = 166981,
+            [ [=[Lady Jaina Proudmoore]=] ] = 166980,
+            [ [=[Baine Bloodhoof]=] ] = 168162,
+            [ [=[The Jailer]=] ] = 165539,   
+        },
+        [1648] = {        
+            [ [=[Anduin Wrynn]=] ] = 167833,      
+        }
     },
-    [1648] = {        
-        [ [=[Anduin Wrynn]=] ] = 167833,      
-    }
+    [ [=[Isle of Quel'Danas]=] ] = {
+        [0] = {
+            [ [=[Arator]=] ] = 250391,
+        },
+    },      
+    [ [=[Eversong Woods]=] ] = {
+        [0] = {
+            [ [=[Arator]=] ] = 250391,
+            [ [=[Orweyna]=] ] = 236903,            
+        },
+    }, 
+    [ [=[Zul'Aman]=] ] = {
+        [0] = {
+            [ [=[Lady Liadrin]=] ] = 236146,          
+        },
+    },      
 }
-
 
 
